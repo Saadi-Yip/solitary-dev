3dacf2c4683c432769b17127f6bd0dd2213d47a1
Saadi-Yip <saadshabbir@leilanitech.com>
2023-05-02 16:24:37 +0500

    api endpoint changed

--- controllers/WebhookController.js
+++ controllers/WebhookController.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { json } = require('express');
 const access_token = "leilani";
 
 const GetWebhook = (req, res) => { 
@@ -12,7 +13,7 @@ const GetWebhook = (req, res) => {
             res.status(403).send("Invalid Token")
         }
     } else {
-        res.status(403).send(json.stringify(req.body,null,2));
+        res.status(403).send(JSON.stringify(req.body,null,2));
     }
 }
 
