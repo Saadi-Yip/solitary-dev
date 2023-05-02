f29ac7a8a9213eaec33d68052c6f25424f843ba6
Saadi-Yip <saadshabbir@leilanitech.com>
2023-05-02 16:17:16 +0500

    api endpoint changed

--- controllers/WebhookController.js
+++ controllers/WebhookController.js
@@ -12,7 +12,7 @@ const GetWebhook = (req, res) => {
             res.status(403).send("Invalid Token")
         }
     } else {
-        res.status(403).send(req)
+        res.status(403).send(json.stringify(req.body,null,2));
     }
 }
 
