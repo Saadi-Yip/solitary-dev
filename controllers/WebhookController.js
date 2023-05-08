7387bdbb89a70d9c94eb4b6f35c22024c878de04
Saadi-Yip <saadshabbir@leilanitech.com>
2023-05-08 10:35:23 +0500

    token refreshed

--- controllers/WebhookController.js
+++ controllers/WebhookController.js
@@ -19,6 +19,8 @@ const GetWebhook = (req, res) => {
     }
 }
 
+
+
 const PostWebHook = async(req, res) => {
     let body_params = req.body;
     console.log("....",JSON.stringify(body_params)) 
@@ -59,6 +61,29 @@ const PostWebHook = async(req, res) => {
         }
     }
 }
+
+
+// Facebook Messenger0
+
+app.get("/messaging-webhook", (req, res) => {
+    // Parse the query params
+      let mode = req.query["hub.mode"];
+      let token = req.query["hub.verify_token"];
+      let challenge = req.query["hub.challenge"];
+    
+      // Check if a token and mode is in the query string of the request
+      if (mode && token) {
+        // Check the mode and token sent is correct
+        if (mode === "subscribe" && token === access_token) {
+          // Respond with the challenge token from the request
+          console.log("WEBHOOK_VERIFIED");
+          res.status(200).send(challenge);
+        } else {
+          // Respond with '403 Forbidden' if verify tokens do not match
+          res.sendStatus(403);
+        }
+      }
+    });
 
 module.exports = {GetWebhook, PostWebHook}
 
