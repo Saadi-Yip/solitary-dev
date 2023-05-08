73eb241e2f9855a0fc4b64e35b9020b7a415c8f6
Saadi-Yip <saadshabbir@leilanitech.com>
2023-05-08 10:39:16 +0500

    token refreshed

--- controllers/WebhookController.js
+++ controllers/WebhookController.js
@@ -74,7 +74,7 @@ app.get("/messaging-webhook", (req, res) => {
       // Check if a token and mode is in the query string of the request
       if (mode && token) {
         // Check the mode and token sent is correct
-        if (mode === "subscribe" && token === access_token) {
+        if (mode === "subscribe" && token === "leilani-fb") {
           // Respond with the challenge token from the request
           console.log("WEBHOOK_VERIFIED");
           res.status(200).send(challenge);
