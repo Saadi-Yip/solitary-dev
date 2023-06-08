cdcf5c5a15408be3b5d1f4669e10aebf5ee1d170
Saadi-Yip <saadshabbir@leilanitech.com>
2023-06-08 12:37:02 +0500

    solitary dev blogs api

--- models/Blogs.js
+++ models/Blogs.js
@@ -1,13 +1,45 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 let blogsSchema = new mongoose.Schema({
-  title: { 
+  title: {
     type: String,
-    required: [true, "Blog Title is Required!"], 
+    required: [true, "Blog Title is Required!"],
   },
-  createdAt:{
+  status: {
+    type: Boolean,
+    default: false 
+  },
+  content: {
+    type: String,
+    required: [true, "Blog must have Content"]
+  },
+  excerpt: {
+    type: String,
+    required: [true, "Blog Excerpt is Required!"]
+  },
+  read_time: {
+    type: String,
+    required: [true, "Blog must have a read_time"]
+  },
+  meta_keywords: {
+    type: [],
+    required: [true, "Please specify meta keywords for this Blog"]
+  },
+  alt_img: {
+    type: [],
+    required: [true, "Please specify alt_img for this Blog"],
+  },
+  slug: {
+    type: String,
+    required: [true, "Please specify slug for this Blog"]
+  },
+  createdAt: {
     type: Date,
-    default: Date.now(), 
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: false
   },
   blog_image: {
     type: String,
@@ -16,18 +48,9 @@ let blogsSchema = new mongoose.Schema({
   image_id: {
     type: String,
   },
-  tag: {
-    type: String,
-    required: [true, "Blog's Tag is Required"]
-  },
-  author: {
-    type: String,
-    required: [true, "Blog must have an Author"]
-  },
-  content: {
-    type: String,
-    required: [true, "Blog must have Content"]
-  }
+  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
 });
 const Blogs = mongoose.model("Blog", blogsSchema);
 module.exports = Blogs;
--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,24 @@
 const express = require("express");
-const router = express.Router(); 
-const upload = require("../middleware/MulterMiddleware");  
+const router = express.Router();
+const upload = require("../middleware/MulterMiddleware");
+
 //-- *********** Import Controller Functions *********** --// 
-const ContactController = require("../controllers/ContactController"); 
+const ContactController = require("../controllers/ContactController");
 const CategoryController = require("../controllers/CategoryController");
-const PortfolioController = require('../controllers/PortfolioController'); 
+const PortfolioController = require('../controllers/PortfolioController');
 const BlogsController = require('../controllers/BlogsController');
-
+const AuthController = require("../controllers/AuthController");
+const TagsController = require("../controllers/TagsController");
 //!!  ********************* Routes ********************* --// 
-require('dotenv').config() 
+require('dotenv').config()
 
-router.get("/", (req,res) =>{
+router.get("/", (req, res) => {
   res.send("Solitary Dev Api")
 })
+
+// Login Route
+router.post("/login", AuthController.Login);
+
 
 //! *** Contact Us Routes *** !//
 router.route("/contact")
@@ -23,42 +29,43 @@ router.route("/contact/:id")
   .patch(ContactController.update_contact_us) /*** Update Message ***/
   .delete(ContactController.remove_contact_us) /*** Remove Message ***/
 
-  //! *** Category Routes ***!//
-  router.route("/category")
+//! *** Category Routes ***!//
+router.route("/category")
   .get(CategoryController.get_category) /*** Get all Category ***/
   .post(CategoryController.input_category) /*** Add New Category ***/
 router.route("/category/:id")
   .get(CategoryController.edit_category) /*** Get a Single Category ***/
   .patch(CategoryController.update_category) /*** Update Category ***/
   .delete(CategoryController.remove_category) /*** Remove Category ***/
-  
-  
-  //! *** Portfolio Routes ***!//
-  router.get("/project",PortfolioController.get_project) /*** Get all Project ***/
-  router.post("/project", upload.single('image'), PortfolioController.input_project) /*** Add New Project ***/
+
+
+//! *** Portfolio Routes ***!//
+router.get("/project", PortfolioController.get_project) /*** Get all Project ***/
+router.post("/project", upload.single('image'), PortfolioController.input_project) /*** Add New Project ***/
 router.route("/project/:id")
   .get(PortfolioController.edit_project) /*** Get a Single Project ***/
   .patch(upload.single('image'), PortfolioController.update_project) /*** Update Project ***/
   .delete(PortfolioController.remove_project) /*** Remove Project ***/
- 
- 
-  //! *** Blogs Routes ***!//
-  router.get("/blog", BlogsController.get_blog) /*** Get all Blogs ***/
-  router.post("/blog", upload.single('blog'), BlogsController.add_blog) /*** Add New Blogs ***/
+
+
+//! *** Blogs Routes ***!//
+router.get("/blog", BlogsController.get_blog) /*** Get all Blogs ***/
+router.post("/blog", upload.single('blog'), BlogsController.add_blog) /*** Add New Blogs ***/
 router.route("/blog/:id")
   .get(BlogsController.single_blog) /*** Get a Single Blogs ***/
   .patch(upload.single('blog'), BlogsController.update_blog) /*** Update Blogs ***/
   .delete(BlogsController.remove_blog) /*** Remove Blogs ***/
 
-   
-  
- 
 
- 
-
+  //! *** Tags Routes ***!//
+router.get("/tag", TagsController.get_tag) /*** Get all Blogs ***/
+router.post("/tag", TagsController.add_tag) /*** Add New Blogs ***/
+router.route("/tag/:id")
+  .get(TagsController.edit_tag) /*** Get a Single Blogs ***/
+  .patch(upload.single('tag'), TagsController.update_tag) /*** Update Blogs ***/
+  .delete(TagsController.remove_tag) /*** Remove Blogs ***/
 
 // -- /*** Export all Routes ***/ -- // 
 module.exports = router;
 
 
- 
